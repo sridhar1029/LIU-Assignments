be035f42aef96dc373200217ffc5e3550b579088
Sridhar <adhikarla2sridhar@yahoo.com>
2018-09-19 13:14:07 +0200

    completed a little more

--- Visualization Lab/Lab 2/Lab2Test.R
+++ Visualization Lab/Lab 2/Lab2Test.R
@@ -2,32 +2,29 @@ library(ggplot2)
 library(plotly)
 #Assignment 1
 #Q1
-d = read.csv("olive.csv", header = T, row.names = 1)
-head(d, 10)
+ol = read.csv("olive.csv", header = T, row.names = 1)
+head(ol, 10)
 
-ggplot(d, aes(palmitic, oleic, col = linolenic)) + geom_point()
+ggplot(ol, aes(palmitic, oleic, col = linolenic)) + geom_point()
 
-disc = cut_interval(d$linolenic, 4)
-ggplot(d, aes(palmitic, oleic, col = disc)) + geom_point()
+disc = cut_interval(ol$linolenic, 4)
+ggplot(ol, aes(palmitic, oleic, col = disc)) + geom_point()
 
 
 #Q2
-ggplot(d, aes(palmitic, oleic, col = disc)) + geom_point()
-ggplot(d, aes(palmitic, oleic, size = disc)) + geom_point()
-ggplot(d, aes(palmitic, oleic)) + geom_point() + 
-    geom_spoke(angle = d$linolenic, radius = 40)
-
-
-?geom_spoke()
+ggplot(ol, aes(palmitic, oleic, col = disc)) + geom_point()
+ggplot(ol, aes(palmitic, oleic, size = disc)) + geom_point()
+ggplot(ol, aes(palmitic, oleic)) + geom_point() + 
+    geom_spoke(angle = ol$linolenic, radius = 40)
 
 #Q3
-ggplot(d, aes(oleic, eicosenoic, col = Region)) + geom_point()
+ggplot(ol, aes(oleic, eicosenoic, col = Region)) + geom_point()
 
 
 #Q4
-ggplot(d, aes(d$oleic, d$eicosenoic, col = cut_interval(d$linoleic, 3),
-              shape = cut_interval(d$palmitic, 3), 
-              size = cut_interval(d$palmitoleic, 3))) + geom_point()
+ggplot(ol, aes(ol$oleic, ol$eicosenoic, col = cut_interval(ol$linoleic, 3),
+              shape = cut_interval(ol$palmitic, 3), 
+              size = cut_interval(ol$palmitoleic, 3))) + geom_point()
 
 
 #Q5
@@ -36,18 +33,45 @@ ggplot(d, aes(d$oleic, d$eicosenoic, col = d$Region,
               size = cut_interval(d$palmitoleic, 3))) + geom_point()
 
 #Q6
-p <- plot_ly(d, labels = ~Area, type = 'pie', showlegend = FALSE) %>%
-  layout(title = 'United States Personal Expenditures by Categories in 1960')
+p <- plot_ly(ol, labels = ~Area, type = 'pie', showlegend = FALSE) %>%
+  layout(title = 'Pie Chart Area')
 p
 
 #Q7
-ggplot(d, aes(d$linoleic, d$eicosenoic)) + geom_density2d()
+ggplot(d, aes(ol$linoleic, ol$eicosenoic)) + geom_density2d()
 
 
 #Assignment 2
 #Q1
 library(xlsx)
-d2 = read.xlsx("baseball-2016.xlsx", sheetName = "Sheet1", header = TRUE, 
-               row.names = 1)
-head(d2)
+library(MASS)
+bball = read.xlsx("baseball-2016.xlsx", sheetName = "Sheet1", header = TRUE,
+                  row.names = 1)
+head(bball)
+bball.numeric = scale(bball[,3:27])
+distance = dist(bball.numeric)
+res = isoMDS(distance, k=2)
+coords = res$points
 
+coordsMDS = as.data.frame(coords)
+coordsMDS$name = rownames(coordsMDS)
+coordsMDS$league = bball$League
+plot_ly(coordsMDS, x=~V1, y=~V2, type="scatter", mode = "markers"
+        , hovertext=~name, color= ~league)
+
+?scale
+c = data.frame(x = 1:10, y = 101:110, z = 10001:10010, u = 121:130)
+c.numeric = scale(c)
+c
+c.numeric
+dtemp = dist(c.numeric)
+dtemp
+restemp = isoMDS(dtemp , k=2)
+coordstemp = restemp$points
+coordstemp = as.data.frame(coordstemp)
+coordstemp$name = rownames(coordstemp)
+coordstemp
+plot_ly(coordstemp, x=~V1, y=~V2, type="scatter", mode = "markers"
+        , hovertext=~name)
+?Shepard
+sh =Shepard(c, coordstemp)
